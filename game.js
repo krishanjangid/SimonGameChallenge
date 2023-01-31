d67e086169bd7d8baf1ef4402f317c068a7c88cf
krishanjangid <krishanjangid516@gmail.com>
2023-01-31 12:47:07 +0530

    added sound and animation functionality

---
+++ game.js
@@ -0,0 +1,32 @@
+var  buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio('sounds/'+name+'.mp3');
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $('#'+currentColour).addClass('pressed');
+  setTimeout(function () {
+    $('#'+currentColour).removeClass('pressed');
+  },100);
+}
+
+$('.btn').on('click',function () {
+  var bu = $(this).attr('id');
+  userClickedPattern.push(bu);
+  console.log(userClickedPattern);
+  playSound(bu);
+  animatePress(bu);
+  nextSequence();
+});
