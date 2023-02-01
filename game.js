27afe4015ac766045b2dba8913a7b0906a150e6d
krishanjangid <krishanjangid516@gmail.com>
2023-02-01 12:38:57 +0530

    play game

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
 var  buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
-
+var started = false;
+var level = 0;
 function nextSequence() {
+  var userClickedPattern = [];
   var randomNumber = Math.floor(Math.random()*4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
+  level+=1;
+  $('#level-title').text('level '+level);
+
 }
 
 function playSound(name) {
@@ -28,5 +33,30 @@ $('.btn').on('click',function () {
   console.log(userClickedPattern);
   playSound(bu);
   animatePress(bu);
-  nextSequence();
+  var index = userClickedPattern.length;
+  checkAnswer(index);
 });
+
+$(document).keypress(function (event) {
+  console.log(event.key);
+  if(started == false)
+  {
+    $('#level-title').text('level '+level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function checkAnswer(currentLevel) {
+  if(currentLevel == gamePattern.length)
+  {
+    setTimeout(function () {
+      nextSequence();
+    },1000);
+  }
+  else {
+
+    playSound('wrong');
+    level= 0;
+  }
+}
